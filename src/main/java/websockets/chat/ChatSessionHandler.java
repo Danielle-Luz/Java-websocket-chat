6f8a15b43573d4d52f79c9de164b6a894b707341
Danielle Luz <daniln2209@gmail.com>
2024-05-27 01:22:16 -0300

    feat: create add session to chat method

--- src/main/java/websockets/chat/ChatSessionHandler.java
+++ src/main/java/websockets/chat/ChatSessionHandler.java
@@ -1,12 +1,14 @@
 package websockets.chat;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-
+import java.util.Map;
 import javax.websocket.Session;
 
 public class ChatSessionHandler {
 
+  private final Map<String, List<Session>> sessionsByChatId = new HashMap<>();
   private final List<Session> openSessions = new ArrayList<>();
 
   public void addSession(Session newSession) {
@@ -19,5 +21,9 @@ public class ChatSessionHandler {
 
   public List<Session> getOpenSessions() {
     return this.openSessions;
+  }
+
+  public Map<String, List<Session>> getSessionsByChatId() {
+    return this.sessionsByChatId;
   }
 }
--- src/main/java/websockets/chat/ChatWebsocketService.java
+++ src/main/java/websockets/chat/ChatWebsocketService.java
@@ -1,13 +1,44 @@
 package websockets.chat;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-
+import java.util.Map;
 import javax.websocket.Session;
+import org.json.JSONObject;
 
 public class ChatWebsocketService {
 
-  private final static ChatSessionHandler chatSessionHandler = ChatSessionHandlerFactory.getChatSessionHandler();
+  private static final ChatSessionHandler chatSessionHandler = ChatSessionHandlerFactory.getChatSessionHandler();
+
+  public static void addSessionToChat(
+    String message,
+    Session messageSenderSession
+  ) {
+    JSONObject messageAsJson = new JSONObject(message);
+    String chatId = messageAsJson.getString("chatId");
+
+    Map<String, List<Session>> sessionsByChatId = chatSessionHandler.getSessionsByChatId();
+    List<Session> sessionsInChat = sessionsByChatId.get(chatId);
+
+    if (sessionsInChat == null) {
+      sessionsByChatId.put(
+        chatId,
+        new ArrayList<>(Arrays.asList(messageSenderSession))
+      );
+
+      return;
+    }
+
+    boolean isSessionInList = sessionsInChat
+      .stream()
+      .anyMatch(existentSession -> existentSession == messageSenderSession);
+
+    if (!isSessionInList) {
+      sessionsInChat.add(messageSenderSession);
+    }
+  }
 
   public static void sendMessageToAllSessions(
     String message,
