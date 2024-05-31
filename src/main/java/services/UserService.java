641c86cf96bbab29c09b3ffbd4a67b6adedff0e1
Danielle Luz <daniln2209@gmail.com>
2024-05-31 17:28:24 -0300

    feat: add login method

--- src/main/java/services/UserService.java
+++ src/main/java/services/UserService.java
@@ -8,15 +8,13 @@ import utils.EncryptUtils;
 
 public class UserService {
 
-  public static User createUser(User newUser) {
+  public static User createUser(String username, String password) {
     try {
-      String encryptedPassword = EncryptUtils.encryptValue(
-        newUser.getPassword()
-      );
+      String encryptedPassword = EncryptUtils.encryptValue(password);
 
       String insertUserQuery = String.format(
         "INSERT INTO user(username, password) VALUES ('%s', '%s')",
-        newUser.getUsername(),
+        username,
         encryptedPassword
       );
 
@@ -27,6 +25,35 @@ public class UserService {
         createdRow.getString("username"),
         createdRow.getString("password")
       );
+    } catch (SQLException e) {
+      e.printStackTrace(System.err);
+      return null;
+    }
+  }
+
+  public static User login(String username, String password) {
+    try {
+      String userByUsernameQuery = String.format(
+        "SELECT * FROM user WHERE username = '%s'",
+        username
+      );
+
+      ResultSet foundUser = DatabaseConnector.executeQuery(userByUsernameQuery);
+
+      boolean isProvidedPasswordValid = EncryptUtils.isValueEqualToEncrypted(
+        password,
+        foundUser.getString("password")
+      );
+
+      if (isProvidedPasswordValid) {
+        return new User(
+          foundUser.getInt("id"),
+          foundUser.getString("username"),
+          foundUser.getString("password")
+        );
+      } else {
+        return null;
+      }
     } catch (SQLException e) {
       e.printStackTrace(System.err);
       return null;
