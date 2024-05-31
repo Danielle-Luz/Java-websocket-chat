aeb36fbc04b1ac24df4ab1f3795b94718d1ac5ee
Danielle Luz <daniln2209@gmail.com>
2024-05-31 17:42:19 -0300

    feat: add get user as json method and created register user servlet

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,14 +1,28 @@
 package models;
 
+import org.json.JSONObject;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class User {
   private int id;
   private String username;
   private String password;
+
+  public JSONObject getJson() {
+    JSONObject userAsJson = new JSONObject();
+
+    userAsJson.put("id", this.id);
+    userAsJson.put("username", this.username);
+    userAsJson.put("password", this.password);
+
+    return userAsJson;
+  }
 }
---
+++ src/main/java/servlets/CreateUser.java
@@ -0,0 +1,33 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import models.User;
+import services.UserService;
+
+@WebServlet("/createUser")
+public class CreateUser extends HttpServlet {
+
+  public void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+    String username = request.getParameter("username");
+    String password = request.getParameter("password");
+
+    User createdUser = UserService.createUser(username, password);
+
+    if(createdUser == null) {
+      response.sendError(500);
+      response.getWriter().write("{'message': 'An error ocurred and the user was not created'}");
+      return;
+    }
+
+    response.setStatus(201);
+    response.setContentType("application/json");
+    response.setCharacterEncoding("UTF-8");
+    response.getWriter().write(createdUser.getJson().toString());
+  }
+}
