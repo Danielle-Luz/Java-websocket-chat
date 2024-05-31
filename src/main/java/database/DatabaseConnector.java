b8a38fbee66c73b15c3763bd93e9f40e9f65925d
Danielle Luz <daniln2209@gmail.com>
2024-05-31 11:02:41 -0300

    feat: created database connector and its starting method

---
+++ src/main/java/database/DatabaseConnector.java
@@ -0,0 +1,41 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DatabaseConnector {
+
+  private static final String databaseUrl = "jdbc:sqlite:easychat.db";
+
+  public static void createTables() {
+    try (Connection connection = DriverManager.getConnection(databaseUrl)) {
+      Statement statement = connection.createStatement();
+
+      statement.setQueryTimeout(30);
+
+      statement.executeUpdate("drop table if exists user");
+      statement.executeUpdate(
+        "CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT, username VARCHAR NOT NULL, password VARCHAR NOT NULL);"
+      );
+
+      statement.executeUpdate("drop table if exists chat");
+      statement.executeUpdate(
+        "CREATE TABLE chat (id INTEGER PRIMARY KEY AUTOINCREMENT, creator_id INTEGER NOT NULL, FOREIGN KEY(creator_id) REFERENCES user(id));"
+      );
+
+      statement.executeUpdate("drop table if exists chat_members");
+      statement.executeUpdate(
+        "CREATE TABLE chat_members (id INTEGER PRIMARY KEY AUTOINCREMENT, member_id INTEGER NOT NULL, chat_id INTEGER NOT NULL, FOREIGN KEY(member_id) REFERENCES user(id), FOREIGN KEY(chat_id) REFERENCES chat(id));"
+      );
+
+      statement.executeUpdate("drop table if exists message");
+      statement.executeUpdate(
+        "CREATE TABLE message (id INTEGER PRIMARY KEY AUTOINCREMENT, sender_id INTEGER NOT NULL, chat_id INTEGER NOT NULL, FOREIGN KEY(sender_id) REFERENCES user(id), FOREIGN KEY(chat_id) REFERENCES chat(id));"
+      );
+    } catch (SQLException e) {
+      e.printStackTrace(System.err);
+    }
+  }
+}
